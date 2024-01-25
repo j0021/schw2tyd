acbc2cd99fb8372800c3f97cb511d1ade71a394c
j0021 <154912577+j0021@users.noreply.github.com>
2024-01-24 20:16:03 -0600

    Add setup.py

--- schw2tyd.py
+++ schw2tyd.py
@@ -29,31 +29,37 @@
 import csv
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: \n\n schw2tyd.py <schwab csv export file>\n")
-    sys.exit(0)
 
-schw_export_file = "schw_export.csv" # cleaned csv export file name, created by the script
-tyd_import_file = "tyd_import.csv" # file to use to bulk upload to Track Your Dividends
+def main():
 
-# This is to clean up the first 2 and last 2 lines of the schwab csv export file
-with open(sys.argv[1], "r", newline="", encoding="utf-8") as raw_csv_file:
-    raw_csv_data = raw_csv_file.readlines()
-    with open(schw_export_file, "w", newline="", encoding="utf-8") as src_csv_file:
-        src_csv_file.writelines(raw_csv_data[2:-2])
+    if len(sys.argv) != 2:
+        print("Usage: \n\n schw2tyd.py <schwab csv export file>\n")
+        sys.exit(0)
 
-with open(schw_export_file, "r", newline="", encoding="utf-8") as src_csv_file:
-    csv_reader = csv.DictReader(src_csv_file)
-    src_csv_data = []
-    for row in csv_reader:
-        # Positions settings must include the following columns
-        try:
-            ticker, shares, cost = row["Symbol"], row["Quantity"], row["Cost/Share"][1:]
-        except KeyError as e:
-            print("\nerror: the following column was not found: {}\n".format(e))
-            sys.exit(1)
-        src_csv_data.append([ticker, shares, cost])
+    schw_export_file = "schw_export.csv" # cleaned csv export file name, created by the script
+    tyd_import_file = "tyd_import.csv" # file to use to bulk upload to Track Your Dividends
 
-with open(tyd_import_file, "w", newline="", encoding="utf-8") as dst_csv_file:
-    csv_writer = csv.writer(dst_csv_file)
-    csv_writer.writerows(src_csv_data)
+    # This is to clean up the first 2 and last 2 lines of the schwab csv export file
+    with open(sys.argv[1], "r", newline="", encoding="utf-8") as raw_csv_file:
+        raw_csv_data = raw_csv_file.readlines()
+        with open(schw_export_file, "w", newline="", encoding="utf-8") as src_csv_file:
+            src_csv_file.writelines(raw_csv_data[2:-2])
+
+    with open(schw_export_file, "r", newline="", encoding="utf-8") as src_csv_file:
+        csv_reader = csv.DictReader(src_csv_file)
+        src_csv_data = []
+        for row in csv_reader:
+            # Positions settings must include the following columns
+            try:
+                ticker, shares, cost = row["Symbol"], row["Quantity"], row["Cost/Share"][1:]
+            except KeyError as e:
+                print("\nerror: the following column was not found: {}\n".format(e))
+                sys.exit(1)
+            src_csv_data.append([ticker, shares, cost])
+
+    with open(tyd_import_file, "w", newline="", encoding="utf-8") as dst_csv_file:
+        csv_writer = csv.writer(dst_csv_file)
+        csv_writer.writerows(src_csv_data)
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="schw2tyd",
+    version="0.0.1",
+    py_modules=["schw2tyd"],
+    entry_points={
+        "console_scripts":[
+            "schw2tyd=schw2tyd:main"
+        ]
+    }
+)
